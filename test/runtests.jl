37093eedb2c279bc85ee257a47727922fbcbe847
MaximeBouton <maxim.bouton@gmail.com>
2018-10-29 09:41:52 -0700

    improve tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,15 @@ using Test
 include("test_env.jl")
 rng = MersenneTwister(1)
 
+function evaluate(mdp, policy, rng, n_ep=100, max_steps=100)
+    avg_r = 0.
+    sim = RolloutSimulator(rng=rng, max_steps=max_steps)
+    for i=1:n_ep
+        avg_r += simulate(sim, mdp, policy)
+    end
+    return avg_r/=n_ep
+end
+
 @testset "vanilla DQN" begin 
     mdp = TestMDP((5,5), 4, 6)
     model = Chain(x->flattenbatch(x), Dense(100, 8), Dense(8, n_actions(mdp)))
@@ -19,8 +28,7 @@ rng = MersenneTwister(1)
                                  rng=rng)
 
     policy = solve(solver, mdp)
-    sim = RolloutSimulator(rng=rng, max_steps=10)
-    r_basic = simulate(sim, mdp, policy)
+    r_basic = evaluate(mdp, policy, rng)
     @test r_basic >= 1.5
 end
 
@@ -33,8 +41,7 @@ end
                                  rng=rng)
 
     policy = solve(solver, mdp)
-    sim = RolloutSimulator(rng=rng, max_steps=10)
-    r_double = simulate(sim, mdp, policy)
+    r_double =  evaluate(mdp, policy, rng)
     @test r_double >= 1.5
 end
 
@@ -48,8 +55,7 @@ end
                                  rng=rng)
 
     policy = solve(solver, mdp)
-    sim = RolloutSimulator(rng=rng, max_steps=10)
-    r_duel = simulate(sim, mdp, policy)
+    r_duel =  evaluate(mdp, policy, rng)
     @test r_duel >= 1.5
 end
 
@@ -63,8 +69,7 @@ end
                                  rng=rng)
 
     policy = solve(solver, mdp)
-    sim = RolloutSimulator(rng=rng, max_steps=10)
-    r_ddqn = simulate(sim, mdp, policy)
+    r_ddqn =  evaluate(mdp, policy, rng)
     @test r_ddqn >= 1.5
 end
 
@@ -72,15 +77,14 @@ end
 
 @testset "TestMDP DRQN" begin
     mdp = TestMDP((5,5), 1, 6)
-    model = Chain(x->flattenbatch(x), LSTM(25, 32), Dense(32, n_actions(mdp)))
+    model = Chain(x->flattenbatch(x), LSTM(25, 8), Dense(8, n_actions(mdp)))
     solver = DeepQLearningSolver(qnetwork = model, max_steps=10000, learning_rate=0.005, 
                                  eval_freq=2000,num_ep_eval=100,
                                  log_freq = 500, 
-                                 double_q = false, dueling=false, recurrence=true,
+                                 double_q = true, dueling=false, recurrence=true,
                                  rng=rng)
     policy = solve(solver, mdp)
-    sim = RolloutSimulator(rng=rng, max_steps=10)
-    r_drqn = simulate(sim, mdp, policy)
+    r_drqn =  evaluate(mdp, policy, rng)
     @test r_drqn >= 1.5
 end
 
@@ -92,7 +96,7 @@ end
 
     policy = solve(solver, mdp)
     sim = RolloutSimulator(rng=rng, max_steps=10)
-    r_drqn = simulate(sim, mdp, policy)
+    r_drqn =  evaluate(mdp, policy, rng)
     @test r_drqn >= 0.
 end
 
